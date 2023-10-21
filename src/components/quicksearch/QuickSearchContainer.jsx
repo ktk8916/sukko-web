406453983fd2d4d0290ce7b88f1cf66a40219735
ktk8916 <ktk8916@naver.com>
2023-10-22 08:07:56 +0900

    단위 표시

--- src/components/quicksearch/QuickSearchContainer.jsx
+++ src/components/quicksearch/QuickSearchContainer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box, Grid, Paper } from "@mui/material";
+import { Box, Chip, Grid, Paper, Typography } from "@mui/material";
 import SummaryCard from "./SummaryCard";
 import NotFoundNotice from "./NotFoundNotice";
-
+import PersonIcon from "@mui/icons-material/Person";
+import GroupsIcon from "@mui/icons-material/Groups";
 const QuickSearchContainer = ({
   searchNickname,
   taxidermys,
@@ -18,6 +19,8 @@ const QuickSearchContainer = ({
     >
       <Grid container sx={{ width: "70%" }} spacing={3}>
         <Grid item xs={12} md={6}>
+          <Chip avatar={<PersonIcon />} label="캐릭터" variant="outlined" />
+
           {taxidermys.length === 0 && (
             <NotFoundNotice searchNickname={searchNickname} />
           )}
@@ -26,6 +29,7 @@ const QuickSearchContainer = ({
           ))}
         </Grid>
         <Grid item xs={12} md={6}>
+          <Chip avatar={<GroupsIcon />} label="원정대" variant="outlined" />
           {relationTaxidermys.length === 0 && (
             <NotFoundNotice searchNickname={searchNickname} />
           )}
