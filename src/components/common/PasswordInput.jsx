1ccc2ccc00c208f9bfa7c4f9eea277ceb291d5ce
ktk8916 <ktk8916@naver.com>
2023-10-26 04:10:54 +0900

    비밀번호 입력 창 분리

---
+++ src/components/common/PasswordInput.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+const PasswordInput = ({
+  password,
+  passwordChangeHandler,
+  margin = "none",
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <FormControl variant="outlined" margin={margin}>
+      <InputLabel htmlFor="password">비밀번호</InputLabel>
+      <OutlinedInput
+        id="password"
+        value={password}
+        onChange={passwordChangeHandler}
+        type={showPassword ? "text" : "password"}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+        label="Password"
+      />
+    </FormControl>
+  );
+};
+
+export default PasswordInput;
