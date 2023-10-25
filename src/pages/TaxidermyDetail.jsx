bd6801077f9bea34af6c05ae17fbaf89dc9131ae
ktk8916 <ktk8916@naver.com>
2023-10-26 04:10:41 +0900

    수정, 삭제 기능 활성화

--- src/pages/TaxidermyDetail.jsx
+++ src/pages/TaxidermyDetail.jsx
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Grid, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  TextField,
+} from "@mui/material";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { getApi } from "../services/apiClient";
+import { deleteApi, getApi, postApi } from "../services/apiClient";
 import moment from "moment/moment";
 import DetailNotice from "../components/detail/DetailNotice";
+import PasswordInput from "../components/common/PasswordInput";
 
 const TaxidermyDetail = () => {
+  // 글 번호
   const { id } = useParams();
-  const nav = useNavigate();
+
+  // 글 본문
   const [taxidermy, setTaxidermy] = useState({
     id: 0,
     nickname: "",
@@ -17,6 +30,38 @@ const TaxidermyDetail = () => {
     createdAt: "",
     character: { classType: "", name: "", serverType: "" },
   });
+
+  // use
+  const nav = useNavigate();
+
+  // 비밀번호 입력창
+  const [password, setPassword] = useState("");
+
+  // 비밀번호 모달
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // 삭제 비밀번호 확인
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const openDeletePopup = () => {
+    setDeleteOpen(true);
+  };
+  const closeDeletePopup = () => {
+    setPassword("");
+    setDeleteOpen(false);
+  };
+
+  const [editOpen, seteditOpen] = useState(false);
+  const openEditPopup = () => {
+    seteditOpen(true);
+  };
+  const closeEditPopup = () => {
+    setPassword("");
+    seteditOpen(false);
+  };
+
+  // api
   const getTaxidermy = async () => {
     const response = await getApi(`/api/v1/taxidermy/${id}`);
 
@@ -24,8 +69,30 @@ const TaxidermyDetail = () => {
       alert(response.data.message);
       nav("/");
     }
-    console.log(response.data);
     setTaxidermy(response.data);
+  };
+
+  const deleteTaxidermy = async () => {
+    const response = await deleteApi(`/api/v1/taxidermy/${id}`, { password });
+
+    if (response.data !== "") {
+      alert(response.data.message);
+      closeDeletePopup();
+      return;
+    }
+    nav("/");
+  };
+
+  const checkPassword = async () => {
+    const response = await postApi(`/api/v1/taxidermy/${id}`, { password });
+
+    if (response.data !== "") {
+      alert(response.data.message);
+      closeEditPopup();
+      return;
+    }
+
+    nav(`/taxidermy/${id}/update`, { state: { password } });
   };
 
   useEffect(() => {
@@ -33,101 +100,178 @@ const TaxidermyDetail = () => {
   }, []);
 
   return (
-    <>
-      <Box
-        sx={{
-          marginTop: "20px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Grid container sx={{ maxWidth: "1400px" }} spacing={2}>
-          <Grid item md={12} xs={12}>
-            <DetailNotice />
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              id="title"
-              label="제목"
-              variant="outlined"
-              disabled
-              fullWidth
-              value={taxidermy.title}
-            />
-          </Grid>
-          <Grid item md={6} xs={6}>
-            <TextField
-              id="dungeon"
-              label="레이드"
-              variant="outlined"
-              disabled
-              fullWidth
-              value={taxidermy.dungeonType}
-            />
-          </Grid>
-          <Grid item md={6} xs={6}>
-            <TextField
-              id="server"
-              label="서버"
-              variant="outlined"
-              disabled
-              fullWidth
-              value={taxidermy.character.serverType}
-            />
-          </Grid>
-          <Grid item md={3} xs={6}>
-            <TextField
-              id="class"
-              label="직업"
-              variant="outlined"
-              disabled
-              fullWidth
-              value={taxidermy.character.classType}
-            />
-          </Grid>
-
-          <Grid item md={5} xs={6}>
-            <TextField
-              id="nickname"
-              label="숙코 닉네임"
-              variant="outlined"
-              fullWidth
-              value={taxidermy.character.name}
-              disabled
-            />
-          </Grid>
-          <Grid item md={3} xs={8}>
-            <TextField
-              id="createdAt"
-              label="작성일"
-              variant="outlined"
-              disabled
-              fullWidth
-              value={moment(taxidermy.createdAt).format("YYYY-MM-DD HH:mm")}
-            />
-          </Grid>
-          <Grid item md={1} xs={2}>
-            <Link to="/">
-              <Button sx={{ height: "100%" }} variant="outlined">
-                뒤로가기
-              </Button>
-            </Link>
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <TextField
-              id="content"
-              label="내용"
-              disabled
-              multiline
-              rows={12}
-              value={taxidermy.content}
-              fullWidth
-            />
-          </Grid>
+    <Box
+      sx={{
+        marginTop: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid container sx={{ maxWidth: "1400px" }} spacing={2}>
+        <Grid item md={12} xs={12}>
+          <DetailNotice />
         </Grid>
-      </Box>
-    </>
+        <Grid item md={2} xs={12}>
+          <TextField
+            id="dungeon"
+            label="레이드"
+            variant="outlined"
+            InputProps={{
+              readOnly: true,
+            }}
+            fullWidth
+            value={taxidermy.dungeonType}
+          />
+        </Grid>
+        <Grid item md={10} xs={12}>
+          <TextField
+            id="title"
+            label="제목"
+            variant="outlined"
+            multiline
+            InputProps={{
+              readOnly: true,
+            }}
+            fullWidth
+            value={taxidermy.title}
+          />
+        </Grid>
+        <Grid item md={2} xs={6}>
+          <TextField
+            id="server"
+            label="서버"
+            variant="outlined"
+            InputProps={{
+              readOnly: true,
+            }}
+            fullWidth
+            value={taxidermy.character.serverType}
+          />
+        </Grid>
+        <Grid item md={2} xs={6}>
+          <TextField
+            id="class"
+            label="직업"
+            variant="outlined"
+            InputProps={{
+              readOnly: true,
+            }}
+            fullWidth
+            value={taxidermy.character.classType}
+          />
+        </Grid>
+
+        <Grid item md={3} xs={12}>
+          <TextField
+            id="nickname"
+            label="숙코 닉네임"
+            variant="outlined"
+            fullWidth
+            value={taxidermy.character.name}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </Grid>
+        <Grid item md={2} xs={12}>
+          <TextField
+            id="createdAt"
+            label="작성일"
+            variant="outlined"
+            InputProps={{
+              readOnly: true,
+            }}
+            fullWidth
+            value={moment(taxidermy.createdAt).format("YYYY-MM-DD HH:mm")}
+          />
+        </Grid>
+        <Grid item md={1} xs={4}>
+          <Button
+            sx={{ height: "100%", width: "100%" }}
+            variant="outlined"
+            onClick={openEditPopup}
+          >
+            수정
+          </Button>
+        </Grid>
+        <Grid item md={1} xs={4}>
+          <Button
+            sx={{ height: "100%", width: "100%" }}
+            variant="outlined"
+            onClick={openDeletePopup}
+          >
+            삭제
+          </Button>
+          <Dialog
+            open={deleteOpen}
+            onClose={closeDeletePopup}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"비밀번호 확인"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                <PasswordInput
+                  password={password}
+                  passwordChangeHandler={passwordChangeHandler}
+                  margin="normal"
+                />
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={deleteTaxidermy}>삭제</Button>
+              <Button onClick={closeDeletePopup} autoFocus>
+                취소
+              </Button>
+            </DialogActions>
+          </Dialog>
+          <Dialog
+            open={editOpen}
+            onClose={closeEditPopup}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"비밀번호 확인"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                <PasswordInput
+                  password={password}
+                  passwordChangeHandler={passwordChangeHandler}
+                  margin="normal"
+                />
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={checkPassword}>수정</Button>
+              <Button onClick={closeEditPopup} autoFocus>
+                취소
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Grid>
+        <Grid item md={1} xs={4}>
+          <Link to="/">
+            <Button sx={{ height: "100%", width: "100%" }} variant="outlined">
+              뒤로가기
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item md={12} xs={12}>
+          <TextField
+            id="content"
+            label="내용"
+            InputProps={{
+              readOnly: true,
+            }}
+            multiline
+            rows={12}
+            value={taxidermy.content}
+            fullWidth
+          />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
