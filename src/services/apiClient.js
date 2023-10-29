18a6bb24a1700742bcd91b748d5ee3dea7710840
ktk8916 <ktk8916@naver.com>
2023-10-29 14:31:29 +0900

    api 설정

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" && "http://localhost:8080";
 
 const getApi = async (url) => {
   return axios({
